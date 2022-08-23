bd1e9fd474fa081aebd7b059b6cf63107a308de1
zhuwei <763631554@qq.com>
2022-08-23 14:36:49 +0800

    admiral

--- vltava-server/src/main/java/com/dmall/vltava/VltavaApplication.java
+++ vltava-server/src/main/java/com/dmall/vltava/VltavaApplication.java
@@ -1,10 +1,12 @@
 package com.dmall.vltava;
 
+import com.dmall.admiral.client.springboot.AdmiralSpringbootConfigurationInitializer;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @MapperScan("com.dmall.vltava.mapper")
 @EnableDubbo(scanBasePackages = {"com.dmall.vltava.service.remote"})
+@Import(AdmiralSpringbootConfigurationInitializer.class)
 public class VltavaApplication {
 
     public static void main(String[] args) {
